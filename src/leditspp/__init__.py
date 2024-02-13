4a020bfb2866a86da8b0142aaacce8416d005ace
Manuel Brack <manuelbrack@gmx.de>
2024-02-13 18:43:13 +0100

    Update __init__.py

--- src/leditspp/__init__.py
+++ src/leditspp/__init__.py
@@ -1,1 +1,8 @@
+from diffusers.utils import is_torch_available, is_transformers_available
 
+
+
+if is_transformers_available() and is_torch_available():
+    from .pipeline_stable_diffusion_ledits import StableDiffusionPipeline_LEDITS
+    from .pipeline_stable_diffusion_xl_ledits import StableDiffusionPipelineXL_LEDITS
+    from .pipeline_if_ledits import IFDiffusion_LEDITS
